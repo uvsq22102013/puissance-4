5ed71f90c407a15f4a4517ca2a95463736fc9aa3
edgar.baghdasaryan@ens.uvsq.fr <edgar.baghdasaryan@ens.uvsq.fr>
2022-05-08 18:17:34 +0200

    fini

--- Puissance_4_matrices.py
+++ Puissance_4_matrices.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import*
-import time
 import random
+import copy
 
 HEIGHT = 530
 WIDTH = 600
 ec = 15
 obj = []
 player = 0
+player2 = 0
 liste_couleurs = []
 a = WIDTH//7
 variable_efface = []
@@ -20,6 +21,7 @@ canvas = tk.Canvas(racine, height = HEIGHT, width=WIDTH, bg = "blue")
 canvas.pack(side="left")
 
 def create_oval():
+    """fonction créant des ronds blancs représentant les emplacements des pions"""
     l = []
     lc = []
     for n in range(6):
@@ -30,10 +32,10 @@ def create_oval():
         liste_couleurs.append(lc)
         l = []
         lc = []
-
 create_oval()
 
 def joueur():
+    """fonction choisissant aléatoirement le joueur qui commence"""
     global player
     player = random.randint(1,2)
     if player%2 == 0:
@@ -43,24 +45,9 @@ def joueur():
     label3 = tk.Label(text = txt + " " + "commence")
     label3.pack(side = "top")
 
-
-def couleur(x):
-    global obj, liste_couleurs, player, variable_efface
-    if player%2 == 0:
-        col = "red"
-        y = 1
-    else:
-        col = "yellow"
-        y = 2
-    player += 1
-    b = 5
-    while liste_couleurs[b][x] != 0:
-        b -=1
-    canvas.itemconfigure(obj[b][x], fill = col)
-    variable_efface.append([b,x])
-    liste_couleurs[b][x] = y
-
 def clic(event):
+    """fonction repérant la position du clic pour appeler la fonction servant à placer le pion
+    et la fonction qui repère le gagnant (s'il y en a un)"""
     global player
     if cond_start == True:
         if 0 < event.x < 70:
@@ -85,10 +72,30 @@ def clic(event):
             couleur(6)
             gagnant()
 
+def couleur(x):
+    """fonction définissant la couleur du pion en alternant entre rouge et jaune,
+    puis elle repère un emplacement vide dans la colonne afin d'y afficher le pion déposé par le joueur"""
+    global obj, liste_couleurs, player, variable_efface
+    if player%2 == 0:
+        col = "red"
+        y = 1
+    else:
+        col = "yellow"
+        y = 2
+    player += 1
+    b = 5
+    while liste_couleurs[b][x] != 0:
+        b -=1
+    canvas.itemconfigure(obj[b][x], fill = col)
+    variable_efface.append([b,x])
+    liste_couleurs[b][x] = y
+
 def gagnant():
+    """fonction compteur qui appelle la fonction gagnant s'il y a au moins 4 pions de la même couleur alignés"""
     global liste_couleurs
     cpt_r = 0
     cpt_j = 0
+    ##compteur de gagnant en ligne
     for i in range(6):
         for j in range(7):
             if liste_couleurs[i][j] == 1:
@@ -105,6 +112,7 @@ def gagnant():
                 gagnant_jaune()
         cpt_r = 0
         cpt_j = 0
+    ##compteur de gagnant en colonne
     for i in range(7):
         for j in range(6):
             if liste_couleurs[j][i] == 1:
@@ -121,6 +129,7 @@ def gagnant():
                 gagnant_jaune()
         cpt_r = 0
         cpt_j = 0
+    ##compteur de gagant en diagonale allant de la gauche vers la droite
     for i in range(3):
         for j in range(6-i):
             if liste_couleurs[i+j][0+j] == 1:
@@ -137,6 +146,7 @@ def gagnant():
                 gagnant_jaune()
         cpt_r = 0
         cpt_j = 0
+    ##compteur de gagnant en diagonale allant de la gauche vers la droite
     for i in range(3):
         for j in range(6-i):
             if liste_couleurs[j][1+i+j] == 1:
@@ -153,6 +163,7 @@ def gagnant():
                 gagnant_jaune()
         cpt_r = 0
         cpt_j = 0
+    ##compteur de gagnant en diagonale allant de la droite vers la gauche
     for i in range(3):
         for j in range(6):
             if liste_couleurs[j][(5-i)-j] == 1:
@@ -169,9 +180,59 @@ def gagnant():
                 gagnant_jaune()
         cpt_r = 0
         cpt_j = 0
-
+    ##compteur de gagnant en diagonale allant de la droite vers la gauche
+    for i in range(3):
+        for j in range(6-i):
+            if liste_couleurs[j+i][6-j] == 1:
+                cpt_r += 1
+            else :
+                cpt_r = 0
+            if liste_couleurs[j+i][6-j] == 2:
+                cpt_j += 1
+            else :
+                cpt_j = 0
+            if cpt_r == 4:
+                gagnant_rouge()
+            if cpt_j == 4:
+                gagnant_jaune()
+        cpt_r = 0
+        cpt_j = 0
     
+def sauvegarde():
+    global liste_couleurs, player, player2
+    player2 = player
+    fic = open("f_texte", "w")
+    for i in range(len(liste_couleurs)):
+        for j in range(len(liste_couleurs[0])):
+            fic.write(str(liste_couleurs[i][j])+ "\n")
+    fic.close
+
+def affiche_sauvegarde():
+    global obj, liste_couleurs, player, cond_start, player2
+    player = player2
+    liste_couleurs2 = []
+    s = 0
+    fic= open("f_texte", "r")
+    for l in fic:
+        a = int(l)
+        liste_couleurs2.append(a)
+        if len(liste_couleurs2) == 7:
+            liste_couleurs[s] = copy.deepcopy(liste_couleurs2)
+            liste_couleurs2 = []
+            s += 1
+    for i in range(6):
+        for j in range(7):
+            if liste_couleurs[i][j] == 1:
+                col = "red"
+            elif liste_couleurs[i][j] == 2:
+                col = "yellow"
+            else:
+                col = "white"
+            canvas.itemconfigure(obj[i][j], fill = col)
+    cond_start = True
+                
 def start():
+    """enregistrement des noms des joueurs"""
     global name_P1,name_P2, cond_start
     name_P1 = (texte1.get(1.0, END))
     name_P2 = (texte2.get(1.0, END))
@@ -179,12 +240,14 @@ def start():
     joueur()
 
 def efface():
+    """fonction effaçant le dernier pion déposé"""
     global variable_efface, obj
     canvas.itemconfigure(obj[variable_efface[-1][0]][variable_efface[-1][1]], fill = "white")
     liste_couleurs[variable_efface[-1][0]][variable_efface[-1][1]] = 0
     del variable_efface[-1]
 
 def new_game():
+    """fonction remettant tout à 0 afin de pouvoir rejouer"""
     global liste_couleurs, obj, player
     player = 0
     liste_couleurs = []
@@ -192,12 +255,14 @@ def new_game():
     create_oval()
 
 def gagnant_rouge():
+    """affichage du nom du gagant ayant les pions rouges"""
     global cond_start
     cond_start = False
     label_gagnant = tk.Label(text = "Le gagnant est" + " " + name_P1)
     label_gagnant.pack(side = "top")
 
 def gagnant_jaune():
+    """affichage du nom du gagnant ayant les pions jaunes"""
     global cond_start
     cond_start = False
     label_gagnant = tk.Label(text = "Le gagnant est" + " " + name_P2)
@@ -207,18 +272,21 @@ texte1 = Text(racine, height = 1, width = 15)
 texte2 = Text(racine, height = 1, width = 15)
 label1 = tk.Label(text = "Nom joueur 1")
 label2 = tk.Label(text = "Nom joueur 2")
-
 bouton_texte = tk.Button(racine, text = "START", command = start)
-bouton1 = tk.Button(text = "effacer", command = efface)
-bouton2 = tk.Button(text = "restart", command = new_game )
+bouton1 = tk.Button(text = "DELETE LAST", command = efface)
+bouton2 = tk.Button(text = "RESTART", command = new_game )
+bouton3 = tk.Button(text = "SAVE", command = sauvegarde)
+bouton4 = tk.Button(text = "LOAD SAVING", command = affiche_sauvegarde)
 
 bouton_texte.pack(side = "top", pady = 30)
 label1.pack(side = "top")
 texte1.pack(side = "top")
 label2.pack(side = "top")
 texte2.pack(side = "top")
-bouton1.pack(side = "top")
+bouton1.pack(side = "bottom")
 bouton2.pack(side = "top")
+bouton3.pack(side = "bottom")
+bouton4.pack(side = "bottom")
 
 
 canvas.bind("<Button-1>", clic)
